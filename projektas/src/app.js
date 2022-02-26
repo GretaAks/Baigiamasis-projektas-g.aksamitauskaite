213e7ba4d2ac32533d382e75e061dab522b3b516
gretaAks <atataite@gmail.com>
2022-02-27 00:36:03 +0200

    init

---
+++ projektas/src/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import { CssBaseline, ThemeProvider} from '@mui/material';
+
+const App = () => (
+  <ThemeProvider >
+  <CssBaseline>
+    <>
+     pradzia
+    </>
+  </CssBaseline>
+  </ThemeProvider>
+);
+
+export default App;
